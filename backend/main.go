506662b6ff6c966056870668caf0b89680a70d2b
Milanka00 <milankatharangana01@gmail.com>
2024-05-29 18:34:13 +0530

    added backend logs

--- backend/main.go
+++ backend/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"log"
 	"os"
 	"strconv"
 	"sync"
@@ -19,14 +20,14 @@ func main() {
 	})
 	
    
-    http.HandleFunc("/nocache", func(w http.ResponseWriter, r *http.Request) {
+    http.HandleFunc("/responsecache/v1/nocache", func(w http.ResponseWriter, r *http.Request) {
         NoCacheHandler(w, r)
     })
 
-    http.HandleFunc("/privatecache", func(w http.ResponseWriter, r *http.Request) {
+    http.HandleFunc("/responsecache/v1/privatecache", func(w http.ResponseWriter, r *http.Request) {
         PrivateCacheHandler(w, r)
     })
-    http.HandleFunc("/getresponse", func(w http.ResponseWriter, r *http.Request) {
+    http.HandleFunc("/responsecache/v1/getresponse", func(w http.ResponseWriter, r *http.Request) {
         getresponseWithoutHeaders(w, r)
     })
 
@@ -60,6 +61,7 @@ func PublicCacheHandler(w http.ResponseWriter, r *http.Request) {
 	// 	http.Error(w, "User information missing", http.StatusUnauthorized)
 	// 	return
 	// }
+	log.Println("PublicCacheHandler invoked")
 
 	id, err := strconv.Atoi(r.URL.Path[len("/responsecache/v1/publiccache/"):])
 	if err != nil {
@@ -94,18 +96,23 @@ func authMiddleware(next http.HandlerFunc) http.HandlerFunc {
 
 
 func PrivateCacheHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("PrivateCacheHandler invoked")
     w.Header().Set("Cache-Control", "private, max-age=3600")
-   // w.Write(payload)
+	w.Write([]byte("response is cached as private"))
     // sleepBeforeRespond()
 }
 
 func getresponseWithoutHeaders(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Caching is applied through UI"))
+	log.Println("revalidateCacheHandler invoked")
+	w.Header().Set("Cache-Control", "must-revalidate, max-age=180")
+	w.Write([]byte("HTTP allows caches to reuse stale responses when they are disconnected from the origin server. must-revalidate is a way to prevent this from happening - either the stored response is revalidated with the origin server or a 504 (Gateway Timeout) response is generated."))
 
 }
 
 func NoCacheHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("NoCacheHandler invoked")
     w.Header().Set("Cache-Control", "no-store")
+	w.Write([]byte("response is not cached"))
    // w.Write(payload)
     // sleepBeforeRespond()
 }
